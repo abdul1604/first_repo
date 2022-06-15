09a902767b6c61da289a43ebfe375c009b5130c3
abdul1604 <106082510+abdul1604@users.noreply.github.com>
2022-06-15 12:59:51 +0100

    I added a nav icon to my html code

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
-alert('Hello world');
+let nav = document.querySelector("#navbar"),
+  navIcon = document.querySelector(".nav-icon");
+navIcon.addEventListener("click", (e) => {
+  nav.classList.toggle("show");
+});
